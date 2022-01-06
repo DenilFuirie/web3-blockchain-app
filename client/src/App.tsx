3a08339c6de9307072e26320406ddcc5bdf75aaa
DenilFuirie <rusm9snik2013@yandex.ru>
2022-01-06 17:50:59 +0300

    frontend init

---
+++ client/src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+import React from 'react';
+
+const App = () => (
+  <div className="App">
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+  </div>
+);
+
+export default App;
